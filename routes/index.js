53c12446a94d04ab3e9e48b25f2978612d9b6d4a
jaouad ballat <jaouad.ballat@gmail.com>
2018-03-03 14:57:41 +0000

    Set Up products pagination

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/Product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,52 @@ const router = express.Router();
 
 const Product = require('../models/Product');
 
-router.get('/', function(req, res, next) {
-  Product.find(function(err, products) {
-    if(err) return console.log(err);
-    res.status(200).json(products);
-  })
+router.get('/', function (req, res, next) {
+  let perPage = 3;
+  let page = parseInt(req.query.page) || 0;
+  let pages = 0;
+  let nextUrl = '';
+  let prevUrl = '';
+  Product.count().exec(function (err, count) {
+    Product.find()
+      .limit(perPage)
+      .skip(perPage * page)
+      .exec(function (err, products) {
+        pages = Math.floor(count / perPage);
+        if (page === 0) {
+          res.json({
+            products,
+            currentPage: page,
+            pages,
+            count,
+            prevUrl: ``,
+            nextUrl: `http://localhost:3000/products?page=${page + 1}`
+          })
+
+        } else if (page === pages - 1) {
+          res.json({
+            products: products,
+            currentPage: page,
+            pages,
+            count,
+            prevUrl: `http://localhost:3000/products?page=${page - 1}`,
+            nextUrl: ``
+          })
+        } else if (page > 0 && page < pages) {
+          res.json({
+            products: products,
+            currentPage: page,
+            pages,
+            count,
+            prevUrl: `http://localhost:3000/products?page=${page - 1}`,
+            nextUrl: `http://localhost:3000/products?page=${page + 1}`
+          })
+        }else {
+          res.redirect('/products')
+        }
+
+      });
+  });
 });
 
 router.get('/:id', function (req, res, next) {
